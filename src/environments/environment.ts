3a4fbeda7b520077c46d3173560e4b083657e174
Sophat <sophattann24@gmail.com>
2023-11-24 13:29:43 +0700

    update route

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:8088/api/v1'
+  baseUrl: 'https://api-fintrack.kbaenak.tech/fintrack-client-event-service/api/v1'
 };
 
 /*
