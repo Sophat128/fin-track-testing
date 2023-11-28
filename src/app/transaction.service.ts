84f016733898f731e16a0d210cee05c4fd6f6ca2
Sophat <sophattann24@gmail.com>
2023-11-28 15:27:46 +0700

    update

--- src/app/transaction.service.ts
+++ src/app/transaction.service.ts
@@ -3,7 +3,6 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Transaction } from './models/transaction';
 import { SavingAccount } from './models/savingaccount';
 import { environment } from 'src/environments/environment';
-
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -22,7 +21,6 @@ export class TransactionService {
     });
     return this.http.get(environment.baseUrl  + '/transaction/history/' + accNo, { headers: headers });
   }
-  
   public getUserInfo(userId:any): Observable<any> {
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
