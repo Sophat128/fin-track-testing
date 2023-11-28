6df6fcb0f2c75d0e954b6b7f0d8747417d1f6ec0
Sophat <sophattann24@gmail.com>
2023-11-28 22:08:07 +0700

    update

--- src/app/services/webpush_service/webpush.service.ts
+++ src/app/services/webpush_service/webpush.service.ts
@@ -8,29 +8,29 @@ export class WebPushService {
   constructor(private http: HttpClient) {}
 
   getPublicKey(){
-    return this.http.get('https://api-fintrack.kbaenak.tech/fintrack-web-push-service/api/v1/webpush/vapidKeys');
+    return this.http.get('http://localhost:8084/api/v1/webpush/vapidKeys');
   }
 
   addPushSubscriber(sub: any, userId:string|null) {
     // return this.http.post('http://localhost:9000/api/notifications', sub);
-    return this.http.post('https://api-fintrack.kbaenak.tech/fintrack-web-push-service/api/v1/webpush/subscribe/' + userId, sub);
+    return this.http.post('http://localhost:8084/api/v1/webpush/subscribe/' + userId, sub);
   }
   unsubscribe(endpoint: string) {
     // return this.http.post('http://localhost:9000/api/notifications', sub);
-    return this.http.delete('https://api-fintrack.kbaenak.tech/fintrack-web-push-service/api/v1/webpush/unsubscribe/' + endpoint);
+    return this.http.delete('http://localhost:8084/api/v1/webpush/unsubscribe/' + endpoint);
   }
 
   send(notification: any) {
   
     return this.http.post(
-      'https://api-fintrack.kbaenak.tech/fintrack-web-push-service/api/v1/webpush/send_notification',
+      'http://localhost:8084/api/v1/webpush/send_notification',
       notification
     );
 
   }
   sendToSpecificUser(notification: any, userId:string|null) {
     return this.http.post(
-      'https://api-fintrack.kbaenak.tech/fintrack-web-push-service/api/v1/webpush/send_notification_user/' + userId,
+      'http://localhost:8084/api/v1/webpush/send_notification_user/' + userId,
       notification
     );
 
